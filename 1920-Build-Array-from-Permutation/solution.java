a6b696c71bfa8f51e913a2aab00864eea3b42f65
Sneha Dhobale <dhobalesneha85@gmail.com>
2026-09-13 23:32:53 +0530

    Add LeetCode 1920 solution

---
+++ 1920-Build-Array-from-Permutation/solution.java
@@ -0,0 +1,12 @@
+public class solution{
+    public int[] buildArray(int[] arr){
+        int[] ans = new int[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            ans[i] = nums[nums[i]];
+
+        }
+        return ans;
+
+    } 
+}
