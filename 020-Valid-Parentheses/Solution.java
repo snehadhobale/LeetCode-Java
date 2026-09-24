31952750f95ef3bc97ec83a78677e9357ea7c935
Sneha Dhobale <dhobalesneha85@gmail.com>
2026-09-24 15:31:07 +0530

    Added solution for 20 valid paranthesis

---
+++ 020-Valid-Parentheses/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class Solution{
+    public boolean isValid(String s){
+    Stack<Character> stack = new Stack<>();
+
+    for(char c : s.toCharArray()) {
+        
+        if(c == '(' || c == '[' || c == '{'){
+            stack.push(c);
+        }else{
+            if(stack.isEmpty()){
+                return false;
+
+            }
+            if(c == ')' && stack.peek() == '('){
+                stack.pop();
+            }
+            else if(c == ']' && stack.peek() == '['){
+                stack.pop();
+            }
+            else if(c == '}' && stack.peek() == '{'){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+    return stack.isEmpty();
+    }
+}
