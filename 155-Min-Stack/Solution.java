12ca495846392a7cf1ad2fe1552d6b09a705cd85
Sneha Dhobale <dhobalesneha85@gmail.com>
2026-09-24 16:47:22 +0530

    Added solution for 155 Min Stack

---
+++ 155-Min-Stack/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+class Solution{
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    public void Minstack(){
+
+    }
+
+    public void push(int value){
+        stack.push(value);
+
+        if(minStack.isEmpty() || value <= minStack.peek()) {
+            minStack.push(value);
+
+        }
+    }
+
+    public void pop(){
+        if(stack.peek().equals(minStack.peek())){
+            minStack.pop();
+
+        }
+        stack.pop();
+    
+    }
+
+    public int top(){
+        return stack.peek();
+
+    }
+
+    public int getMin(){
+        return minStack.peek();
+    }
+}
